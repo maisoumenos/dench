758da32b3070c9cb1933cd17258ab369ac1856aa
Thomas Bates <hey@tombates.co>
2017-06-10 21:54:30 -0300

    :bug: Remove console.log

--- components/Breakdown/index.js
+++ components/Breakdown/index.js
@@ -1,8 +1,3 @@
 import { BreakdownList } from './BreakdownList';
-import { BreakdownChange } from './BreakdownChange';
-
-BreakdownList.Change = BreakdownChange;
-
-console.log(BreakdownList.Change);
 
 export default BreakdownList;
